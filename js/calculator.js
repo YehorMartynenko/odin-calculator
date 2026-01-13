c340219e383accbda425169e8077c0725cf8a6cb
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-01-13 11:17:18 +0100

    Add clear and delete buttons functionality

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,6 @@ let secondNumber = "";
 let operator = "";
 
 function sum(a, b){
-    console.log(`Sum: ${a} + ${b}`);
     return a+b;
 }
 
@@ -37,13 +36,49 @@ function operate(a, b, operator){
 let calculationDisplay = document.querySelector(".calculation-textfield");
 let historyDisplay = document.querySelector("#history");
 let buttons = document.querySelectorAll(".keypad-button");
+
 let currentValue = "";
 let result = 0;
+
 buttons.forEach(button => {
 button.addEventListener("click", (e) => {
     console.log(button.className);
+
     switch(button.className) {
+        case "keypad-button delete":
+            if(!operator){
+                firstNumber = firstNumber.toString().slice(0, -1);
+                currentValue = firstNumber;
+                calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+            } else if (operator && !secondNumber){
+                operator = "";
+                calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+            } else if (operator && secondNumber){
+                secondNumber = secondNumber.slice(0, -1);
+                currentValue = secondNumber;
+                calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+            }
+            break;
+        case "keypad-button clear":
+            currentValue = "";
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+            result = 0;
+            while(historyDisplay.firstChild){
+                historyDisplay.removeChild(historyDisplay.firstChild);
+            }
+            calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+            console.log("pressed" + button.className);
+            break;
         case "keypad-button operator equals-sign":
+            if(firstNumber === ""){
+                firstNumber = 0;
+            }
+            if(secondNumber === ""){
+                secondNumber = 0;
+            }
+
             result = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
             calculationDisplay.textContent = `${result}`;
 
@@ -51,28 +86,33 @@ button.addEventListener("click", (e) => {
             historyRecord.classList.add("history-textfield");
             historyRecord.textContent = `${firstNumber} ${operator} ${secondNumber} = ${result}`;
             historyDisplay.appendChild(historyRecord);
+
             firstNumber = result;
             secondNumber = "";
             currentValue = "";
             operator = "";
+
             break;
         case "keypad-button operator":
             if(!operator || (operator && !secondNumber)){
-            operator = button.textContent;
-            currentValue = "";
-            calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-            } else if(operator && secondNumber){
-                result = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
-                let historyRecord = document.createElement("div");
-                historyRecord.classList.add("history-textfield");
-                historyRecord.textContent = `${firstNumber} ${operator} ${secondNumber} = ${result}`;
-                historyDisplay.appendChild(historyRecord);
-                firstNumber = result;
                 operator = button.textContent;
-                secondNumber = "";
                 currentValue = "";
                 calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-            }
+            } else if(operator && secondNumber){
+
+                    result = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
+
+                    let historyRecord = document.createElement("div");
+                    historyRecord.classList.add("history-textfield");
+                    historyRecord.textContent = `${firstNumber} ${operator} ${secondNumber} = ${result}`;
+                    historyDisplay.appendChild(historyRecord);
+
+                    firstNumber = result;
+                    secondNumber = "";
+                    currentValue = "";
+                    operator = button.textContent;
+                    calculationDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+                }
             break;
         case "keypad-button":
             currentValue = currentValue+button.textContent;
@@ -85,5 +125,6 @@ button.addEventListener("click", (e) => {
             break;
     }
     console.log(`current: ${currentValue}\nfirstNumber: ${firstNumber}\noperator: ${operator}\nsecondNumber: ${secondNumber}\nresult: ${result}`);
+    console.log(`fistNumberType = ${typeof firstNumber}`);
     });
 });
